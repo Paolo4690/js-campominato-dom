134ba0b402508a67dd9e3e7b90ed886273b8b8ad
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-03-03 13:47:53 +0100

    add file initial

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const eleBtnPlay = document.querySelector('#play');
+const eleOptDifficolta = document.querySelector('#difficolta');
+const eleContainer = document.querySelector('#container-campo-minato')
+const arrDifficolta =[100, 81, 49]
+
+eleBtnPlay.addEventListener('click', createGrid)
+
+function createGrid() {
+    eleContainer.innerHTML = ''
+    const numBoxRow = Math.sqrt(arrDifficolta[eleOptDifficolta.value])
+
+    for (let i = 1; i <= arrDifficolta[eleOptDifficolta.value]; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `calc(100% / ${numBoxRow})`
+        square.style.height = `calc(100% / ${numBoxRow})`
+        square.innerHTML = i;
+        eleContainer.append(square);
+        square.addEventListener('click', function() {
+            this.classList.add('color-cell')
+        })
+    }
+}
